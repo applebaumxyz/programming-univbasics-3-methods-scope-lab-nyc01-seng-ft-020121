06b3d727a49583240459e38f6295b6bef5105a53
Sean Brown <applebaumxyz@gmail.com>
2021-01-22 18:11:51 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -14,3 +14,12 @@ def link
 	puts phrase
 end
 
+def all_phrases
+	phrase_one = 'It\'s-a me, Mario!'
+	phrase_two = 'Thank You Mario! But Our Princess Is In Another Castle!'
+	phrase_three = 'It\'s Dangerous To Go Alone! Take This.'
+	puts phrase_one
+	puts phrase_two
+	puts phrase_three
+end
+
